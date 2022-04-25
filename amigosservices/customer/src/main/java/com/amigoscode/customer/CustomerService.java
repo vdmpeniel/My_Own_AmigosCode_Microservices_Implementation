02045cbd1f6b1943d3a1aea808473c78dd3a7be8
victor diaz <vdmpeniel@yahoo.es>
2022-04-25 10:55:56 -0400

    Chapter6 - OpenFeign - http clients

--- amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @AllArgsConstructor
 public class CustomerService{
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -24,11 +26,7 @@ public class CustomerService{
         customerRepository.saveAndFlush(customer);
 
         // todo: check if fraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if(fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("fraudster");
